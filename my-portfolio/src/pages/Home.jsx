0134aad341bcc4fc3d0452b66077209b66e79de8
spiderasasen <spiderasasen@gmail.com>
2026-03-05 08:29:07 -0500

    decent disgn but it can be better

--- my-portfolio/src/pages/Home.jsx
+++ my-portfolio/src/pages/Home.jsx
@@ -1,21 +1,22 @@
+import "../styles/home.css"
 
 function Home(){
     return(
       <div>
           {/*for the button*/}
-          <div className="Butto_Header">
+          <div className="nav">
               <button id="Home">Home</button>
               <button id="Projects">Projects</button>
               <button id="Contact">Contact Me</button>
           </div>
 
           {/*for the home screen*/}
-          <div className="main_screen">
+          <div className="main">
               {/*main section for the eye catching section*/}
               <h1>Something Eye Catching</h1>
 
               {/*about me*/}
-              <div className="about_me">
+              <div className="about">
                   <h3>About Me</h3>
                   <p>Something, idk</p>
               </div>
