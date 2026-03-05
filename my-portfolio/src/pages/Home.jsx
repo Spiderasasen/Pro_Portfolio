8083828a5480b0f43ac41f5a72ac61db0e9a311d
spiderasasen <spiderasasen@gmail.com>
2026-03-05 08:15:22 -0500

    made the skeltion for the home menu

--- my-portfolio/src/pages/Home.jsx
+++ my-portfolio/src/pages/Home.jsx
@@ -1,9 +1,25 @@
-import {useState, useEffect} from 'react'
 
 function Home(){
     return(
       <div>
-          <h1>Home</h1>
+          {/*for the button*/}
+          <div className="Butto_Header">
+              <button id="Home">Home</button>
+              <button id="Projects">Projects</button>
+              <button id="Contact">Contact Me</button>
+          </div>
+
+          {/*for the home screen*/}
+          <div className="main_screen">
+              {/*main section for the eye catching section*/}
+              <h1>Something Eye Catching</h1>
+
+              {/*about me*/}
+              <div className="about_me">
+                  <h3>About Me</h3>
+                  <p>Something, idk</p>
+              </div>
+          </div>
       </div>
     );
 }
