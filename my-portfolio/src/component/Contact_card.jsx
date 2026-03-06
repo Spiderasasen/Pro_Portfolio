ecb80d403560ad79af9e7687756edbe79b1da1b8
spiderasasen <spiderasasen@gmail.com>
2026-03-06 15:57:24 -0500

    contact system is done. - data - Contact_card.jsx - Contact.jsx
    each system is made to make life more simpler

--- my-portfolio/src/component/Contact_card.jsx
+++ my-portfolio/src/component/Contact_card.jsx
@@ -1,16 +1,34 @@
 import contact from '../data/contact.json';
 import "../styles/contact.css";
 
-function Contact_card(){
-    return(
-      <div>
-          {contact.map((item) =>(
-              <div key={item.id} className="contact_card">
-                  <h3>{item.Name}</h3>
-                  <a href={item.contact_link}>Link for {item.Name}</a>
-              </div>
-          ))}
-      </div>
+function Contact_card() {
+    return (
+        <div>
+            {contact.map((item) => {
+                // If it's email, build a mailto link
+                const link =
+                    item.Name.toLowerCase() === "email"
+                        ? `mailto:${item.contact_link}`
+                        : item.contact_link;
+
+                return (
+                    <div
+                        key={item.id}
+                        className="contact_card"
+                        onClick={() => window.location.href = link}
+                    >
+                        <h3>{item.Name}</h3>
+                        <a
+                            href={link}
+                            onClick={(e) => e.stopPropagation()}
+                        >
+                            Link for {item.Name}
+                        </a>
+                    </div>
+                );
+            })}
+        </div>
     );
 }
+
 export default Contact_card;
