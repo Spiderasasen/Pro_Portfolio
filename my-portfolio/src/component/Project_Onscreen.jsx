51822487d626b49c5d9660b03be66459b46670f1
spiderasasen <spiderasasen@gmail.com>
2026-03-09 08:18:21 -0400

    made the fitler button layout, and begining on the onscreen card of the projects

---
+++ my-portfolio/src/component/Project_Onscreen.jsx
@@ -0,0 +1,10 @@
+import projects from "../data/projects.json";
+
+function ProjectOnscreen() {
+    return(
+      <div>
+
+      </div>
+    );
+}
+export default ProjectOnscreen;
--- my-portfolio/src/pages/Projects.jsx
+++ my-portfolio/src/pages/Projects.jsx
@@ -12,6 +12,20 @@ function Projects() {
                     <button onClick={() => navigate("/Contact")}>Contact Me</button>
                 </nav>
             </header>
+
+
+            {/*listing out all the projects*/}
+            <section className="hero">
+                <h1>Projects</h1>
+            </section>
+
+            {/*filter for all the projects*/}
+            <div>
+                <button>Personal</button>
+                <button>School Work</button>
+                <button>Work</button>
+                <button>Language</button>
+            </div>
         </div>
     )
 }
