c5551148bce4ddc2beecfe321784553bd3934701
spiderasasen <spiderasasen@gmail.com>
2026-03-09 08:31:02 -0400

    made the design for the card that is shown onscreen

--- my-portfolio/src/component/Project_Onscreen.jsx
+++ my-portfolio/src/component/Project_Onscreen.jsx
@@ -1,19 +1,22 @@
 import projects from "../data/projects.json";
+import "../styles/Onscreen.css"
 
 function ProjectOnscreen() {
-    return(
-      <div>
-          {projects.map((item) => (
-              <div key={item.id}>
-                  <h3>{item.name}</h3>
-                  {/*having both the type of project and the project langauge next to each other*/}
-                  <div>
-                      <p><em>{item.Type_of_Project} in {item.programming_langagues}</em></p>
-                  </div>
-                  <p>{item.description}</p>
-              </div>
-          ))}
-      </div>
+    return (
+        <div className="project-list">
+            {projects.map((item) => (
+                <div className="project-card" key={item.id}>
+                    <h3>{item.name}</h3>
+
+                    <div className="project-meta">
+                        <em>{item.Type_of_Project} project in {item.programming_langagues}</em>
+                    </div>
+
+                    <p>{item.description}</p>
+                </div>
+            ))}
+        </div>
     );
 }
+
 export default ProjectOnscreen;
